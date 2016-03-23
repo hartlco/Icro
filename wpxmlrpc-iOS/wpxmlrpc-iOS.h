5b5e14dd1dc72d198a4ed3db57cab15ac19a8ac5
Jorge Bernal <jbernal@gmail.com>
2016-03-23 16:48:51 +0100

    Add empty iOS/Mac targets

---
+++ wpxmlrpc-iOS/wpxmlrpc-iOS.h
@@ -0,0 +1,19 @@
+//
+//  wpxmlrpc-iOS.h
+//  wpxmlrpc-iOS
+//
+//  Created by Jorge Bernal Ordovas on 23/03/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for wpxmlrpc-iOS.
+FOUNDATION_EXPORT double wpxmlrpc_iOSVersionNumber;
+
+//! Project version string for wpxmlrpc-iOS.
+FOUNDATION_EXPORT const unsigned char wpxmlrpc_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <wpxmlrpc_iOS/PublicHeader.h>
+
+
---
+++ wpxmlrpc-iOSTests/wpxmlrpc_iOSTests.m
@@ -0,0 +1,39 @@
+//
+//  wpxmlrpc_iOSTests.m
+//  wpxmlrpc-iOSTests
+//
+//  Created by Jorge Bernal Ordovas on 23/03/16.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface wpxmlrpc_iOSTests : XCTestCase
+
+@end
+
+@implementation wpxmlrpc_iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
