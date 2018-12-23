083aa4a6539de1a266142e026729fd78ab85c53d
Martin Hartl <martin@hartl.co>
2018-12-23 10:00:26 +0100

    Add basic theme types

--- IcroKit/Helper/Color.swift
+++ IcroKit/Helper/Color.swift
@@ -18,4 +18,66 @@ public struct Color {
     public static let yellow = XColor(named: "yellow")
     public static let accentDark = XColor(named: "accentDark")!
     public static let separatorColor = XColor(named: "separatorColor")!
+
+    public static var backgroundColor: XColor {
+        return Theme.colorTheme.backgroundColor
+    }
+
+    public static var textColor: XColor {
+        return Theme.colorTheme.textColor
+    }
+}
+
+public enum Theme {
+    case light
+    case gray
+    case black
+
+    static let currentTheme = Theme.black
+
+    static var colorTheme: ColorTheme.Type {
+        switch currentTheme {
+        case .black:
+            return BlackTheme.self
+        case .gray:
+            return GrayTheme.self
+        case .light:
+            return LightTheme.self
+        }
+    }
+}
+
+protocol ColorTheme {
+    static var textColor: XColor { get }
+    static var backgroundColor: XColor { get }
+}
+
+struct BlackTheme: ColorTheme {
+    static var textColor: XColor {
+        return .white
+    }
+
+    static var backgroundColor: XColor {
+        return .black
+    }
+}
+
+struct LightTheme: ColorTheme {
+    static var textColor: XColor {
+        return .darkText
+    }
+
+    static var backgroundColor: XColor {
+        return .white
+    }
+}
+
+struct GrayTheme: ColorTheme {
+    static var textColor: XColor {
+        return .lightGray
+    }
+
+    static var backgroundColor: XColor {
+        return .darkGray
+    }
 }
