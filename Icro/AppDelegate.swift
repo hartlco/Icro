dce5f1e35d1b4e00ab04816079f89560ead7f28e
Martin Hartl <martin@hartl.co>
2018-12-24 08:58:33 +0100

    Add Appearance manager

--- Icro/AppDelegate.swift
+++ Icro/AppDelegate.swift
@@ -29,6 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
 
         DiscoveryCategoryManager.shared.update()
+        AppearanceManager().applyAppearance()
 
         return true
     }
